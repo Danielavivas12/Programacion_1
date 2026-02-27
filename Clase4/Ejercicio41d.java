6b1c517aaafbb9240edc9b77e9656957280b1a21
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-02-27 18:56:11 +0000

    Ejercicio41d

---
+++ Clase4/Ejercicio41d.java
@@ -0,0 +1,21 @@
+package Clase4;
+
+import java.util.Scanner;
+
+public class Ejercicio41d {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+       // 'x','y','z'son variables definidas y 'scanner'es un objeto scanner
+       System.out.println("Digite un valor para p...");
+      int valor = sc.nextInt();
+      if (valor x>y && z<20) {
+        System.out.println("Ingrese valor para p:");
+      }else{
+          System.out.println("x>y && z<20");
+      }
+        sc.close();
+        
+    }
+        
+}
