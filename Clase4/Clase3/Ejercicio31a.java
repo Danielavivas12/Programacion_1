778f26177f8142f645041c658bf8fa83120ef79e
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-01 23:59:15 +0000

    Ejercicio31a.java

--- Clase4/Clase3/Ejercicio31a.java
+++ Clase4/Clase3/Ejercicio31a.java
@@ -1,15 +1,17 @@
-package .Clase3;
+package Clase3;
 
 import  java.util.Scanner;
 public class Ejercicio31a {
-}
 Public static void main(String[] arg);
+//paso1: crear objeto para leer entrada del usuario
 Scanner sc = new Scanner (System.in);
-// Area del cuadrado 
-System.out.print("Digite el lado del cuadrado");
+//paso2: solisitar y leer el lado del cuadrado
+
+System.out.print("Digite la longitud del lado del cuadrado:");
 double lado = sc.nextDouble();
 double Area cuadrado= lado*lado;
-System.out.println("Area del cuadrado:"+ areacadrado);
+System.out.println("El area del cuadrado es:"+ Area);
+
 sc.closse(); 
  }
 
