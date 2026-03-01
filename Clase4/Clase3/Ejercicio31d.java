97165d4f8b1c1f28b3dd546041b317df91cb9d79
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-01 22:33:46 +0000

    Ejercicio31d.java

---
+++ Clase4/Clase3/Ejercicio31d.java
@@ -0,0 +1,25 @@
+package Clase3;
+
+import java.util.Scanner;
+
+public class Ejercicio31d {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // calculo del salario neto
+    double salarioBruto = 1750905.0;
+    System.out.println("nsalariobruto(salario minimo 2026):"+ salarioBruto+"cop");
+
+    System.out.print("Digite deduciones:");
+    double deduciones = sc.nextDouble();
+
+    System.out.print("Digite comision:");
+    double comision = sc.nextDouble();
+
+    double salarioneto = (salarioBruto-deduciones)+comision;
+    System.out.println("salario neto:"+salaroneto+"cop");
+
+    sc.close();
+    
+  }
+    
+}
