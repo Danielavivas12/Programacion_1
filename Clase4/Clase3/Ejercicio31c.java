5fa89694b462f88e00e2896a60eec67cc5c24ebd
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-02 00:54:49 +0000

    Ejercicio 31c.java

--- Clase4/Clase3/Ejercicio31c.java
+++ Clase4/Clase3/Ejercicio31c.java
@@ -2,23 +2,21 @@ package Clase3;
 
 import java.util.Scanner;
 public class Ejercicio31c {
-
-}
-public static void main(String[] aStrings){
+public static void main(String[] arg);
 Scanner sc = new Scanner(System.in);
-System.out.print("Digite la temperatura en celsius");
-double celsius = sc.nextDouble();
-double fahrenheit = (celsius*9/5)+32;
-System.out.println(celsius+"°c="+fahrenheit+"°f");
-
-System.out.print("Digite la temperatura faherenheit:");
-fahrenheit = sc.nextDouble();
-celsius = (fahrenheit-32)*5/9;
+System.out.print("Digite la temperatura en Fahrenheit");
+double fahrenheit = sc.nextDouble();
+double celsius = (fahrenheit-32)*5/9;
 System.out.println(fahrenheit+"°f="+celsius+"°c");
 
-} 
-sc.clese();
+System.out.print("Digite la temperatura de celsius:");
+celsius= sc.nextDouble();
+fahrenheit = (celsius*9/5)+32;
+System.out.println(celsius+"°c="+fahrenheit+"°f");
+
+sc.close();
 
 }
 
+}
 
