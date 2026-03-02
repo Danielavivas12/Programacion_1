2dc2e28cffe49bd59f59e383ab6d94d8351a1908
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-02 02:06:02 +0000

    Ejercicio4.4.java

---
+++ Clase4/Ejercicio4.4.java
@@ -0,0 +1,15 @@
+package Clase4;
+
+import java.util.Scanner;
+public class Ejercicio4.4 {
+    public static void main(String[] arg){
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Digitar un numero");
+    double numero = sc.nextDouble();
+
+    sc.close();
+    }
+
+    
+}
