ddf3fbbace80ab54f94dad43ff023349ee7767f8
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-02 00:20:18 +0000

    Ejercicio31b.java

--- Clase4/Clase3/Ejercicio31b.java
+++ Clase4/Clase3/Ejercicio31b.java
@@ -3,14 +3,15 @@ package Clase3
 import java.util.Scanner;
 Public class Ejercicio31b{
     Public static void main(String[] arg);{
-      
-    }
-  //  Area de circulo
+      Scanner sc = new Scanner(System.in);
+      //leer el radio
+      System.out.print("Digite el radio del circulo:");
   double radio = sc.nextDouble();
-  final double PI=Math.PI;
-  double areacirculo=PI*radio*radio;
-  System.out.println("Area del circulo"+areacirculo);
-}
-sc.close();
+  final double PI=3.1416;
+  double area=PI*radio*radio;
+  System.out.printf("El area del circulo es:%2f%n",area);
+  sc.close();
+ }
+
      
 }
