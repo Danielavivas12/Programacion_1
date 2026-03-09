baff53a373ce1ac600c068bceefadd42de8f9fd9
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-09 15:49:31 +0000

    Ejercicio412.java

---
+++ Clase6/Ejercicio412.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+pblic class Ejercicio412{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1 , nim2;
+        in+ seleccion;
+        System.out.println("Digite numero1");
+        num1 = sc.nextInt();
+        System.out.println("Digite numero2");
+        num2 = sc.nextInt();
+        System.out.println("Digite la solucion 1,2!3");
+        seleccion = sc.nextInt();
+        if (seleccion == 1){
+            System.out.primtln("la suma" +(num1+num2));
+    }
