5ae41f9a1a8cf6e30f5320088cd9d67cc5b6b788
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-03-09 16:13:18 +0000

    Ejercicio412.java

--- Clase6/Ejercicio412.java
+++ Clase6/Ejercicio412.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
-pblic class Ejercicio412{
+public class Ejercicio412 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double num1 , nim2;
-        in+ seleccion;
+        double num1, num2;
+        int seleccion;
         System.out.println("Digite numero1");
-        num1 = sc.nextInt();
+        num1 = sc.nextDouble();
         System.out.println("Digite numero2");
-        num2 = sc.nextInt();
-        System.out.println("Digite la solucion 1,2!3");
+        num2 = sc.nextDouble();
+        System.out.println("Digite la solucion 1,2 o 3");
         seleccion = sc.nextInt();
-        if (seleccion == 1){
-            System.out.primtln("la suma" +(num1+num2));
+        if (seleccion == 1) {
+            System.out.println("la suma es: " + (num1 + num2));
+        } else {
+            if (seleccion == 2) {
+                System.out.println("la multiplicación es: " + (num1 * num2));
+            } else {
+                if (seleccion == 3) {
+                    System.out.println("la división es: " + (num1 / num2));
+                } else {
+                    System.out.println("opción no valia");
+                }
+            } // else
+        }
+        sc.close();
     }
+}
