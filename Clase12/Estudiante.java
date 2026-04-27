37376e7e4cec26b2ed8ceb5e75c650e280de294b
Danielavivas12 <dvivaso@estudiante.uniajc.edu.co>
2026-04-27 15:35:09 +0000

    Estudiante.java

--- Clase12/Estudiante.java
+++ Clase12/Estudiante.java
@@ -1,41 +1,42 @@
 package Clase12;
+
 public class Estudiante {
-    
-    //Atributos
+
+    // Atributos
     private int identificacion;
     private String nombres;
     private String apellidos;
     private int edad;
 
-    //Constructor de la clase
-    //El constructor permite inicializar la clase
-    //El constructor se llama igual que la clase
-    //la palabra reservada "this" es una autoreferencia al atributo de la clase
+    // Constructor de la clase
+    // El constructor permite inicializar la clase
+    // El constructor se llama igual que la clase
+    // la palabra reservada "this" es una autoreferencia al atributo de la clase
 
-    public Estudiante(int identificacion, String nombres, String apellidos, int edad){
+    public Estudiante(int identificacion, String nombres, String apellidos, int edad) {
         this.identificacion = identificacion;
         this.nombres = nombres;
         this.apellidos = apellidos;
         this.edad = edad;
     }
 
-    //Métodos get y set para el atributo nombres
-    public String getNombres(){
+    // Métodos get y set para el atributo nombres
+    public String getNombres() {
         return nombres;
     }
 
-    public void setNombres(String nombres){
+    public void setNombres(String nombres) {
         this.nombres = nombres;
     }
 
-    //El método toString permite mostrar la información que tiene el objeto.
-    public String toString(){
-        return "Estudiante { identificación: " + identificacion + 
-               " nombres y apellidos: " + nombres + " " + apellidos + 
-               " edad: " + edad + "}";
+    // El método toString permite mostrar la información que tiene el objeto.
+    public String toString() {
+        return "Estudiante { identificación: " + identificacion +
+                " nombres y apellidos: " + nombres + " " + apellidos +
+                " edad: " + edad + "}";
     }
 
-    public double calcularNotaFinal (double n1, double n2, double n3){
+    public double calcularNotaFinal(double n1, double n2, double n3) {
         double notaFinal = ((n1 * 0.30) + (n2 * 0.30) + (n3 * 0.40));
         return notaFinal;
     }
